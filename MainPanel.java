26cf32c58e4c3cee98e0eec601cc9ffd12094881
ctallwhiteman <ct223134@umconnect.umt.edu>
2017-09-14 10:30:04 -0600

    Delete Extra Buttons
    Deleted buttons

--- MainPanel.java
+++ MainPanel.java
@@ -12,19 +12,18 @@ import java.awt.Font;
 
 public class MainPanel extends JPanel implements ActionListener{
 
-	
+
 	JButton Member1=new JButton("Derek Hickman");
 	JButton Member2=new JButton("Clayton Tallwhiteman");
 	JButton Member3=new JButton("Cordell Appel");
 	JButton Member4=new JButton("David Rich");
-	JButton Member5=new JButton("Garret Blitzman");
-	JButton Member6=new JButton("Merek the Gitman");
-	
+
+
 	Dimension size = new Dimension(600,600);
 	private String text =  "Meet Our Team";
 	public MainPanel()
 	{
-		
+
 		JLabel label = new JLabel (this.text, JLabel.CENTER );
         label.setOpaque(true);
         label.setFont(new Font("Serif", Font.PLAIN, 30));
@@ -35,22 +34,17 @@ public class MainPanel extends JPanel implements ActionListener{
 		add(Member2);
 		add(Member3);
 		add(Member4);
-		add(Member5);
-		add(Member6);
-		
-		
-		
+
+
+
+
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
-	
+
+
 }
-
-
-	
-	
